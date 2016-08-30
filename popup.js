affd1fb221441e59bcc12e493cb755c871de10ae
= <mitalipalekar1@gmail.com>
2016-08-30 16:09:14 -0700

    added text area to input source code because of autocorrection by Chrome browser

--- popup.js
+++ popup.js
@@ -1,24 +1,41 @@
 (function() {
 	"use strict";
 
-
-	var HTMLcodeContent;
-
 	window.onload = function() {
-		HTMLcodeContent = document.getElementById("htmlCode");
-
+		document.getElementById("submithtml").onclick = HTMLCodeGenerator;
+		
+		//had to change strategy because of autocorrection of Chrome browser from HTML to XTML
+		/*
+		HTMLcodeContent = document.getElementById("htmlCodeOutput");
 		chrome.tabs.executeScript(null, {file: "HTMLCodeGenerator.js"}, function() {
 			if(chrome.runtime.lastError){
 				HTMLcodeContent.innerHTML = "HTML generator produced an error: " + chrome.runtime.lastError.message;
 			}
-		});
+		});*/
 	};
 
+	//had to change strategy because of autocorrection of Chrome browser from HTML to XTML
+	/*
 	chrome.runtime.onMessage.addListener(function(message, sender, response) {
 		HTMLcodeContent.innerHTML = message;
-		console.log(HTMLcodeContent.innerHTML);
 		determineValidHTML();
-	});
+	});*/
+
+	function HTMLCodeGenerator() {
+		var htmlCode = document.getElementById("htmlCodeInput");
+		document.getElementById("htmlCodeOutput").innerHTML = escapeChars(htmlCode.value);
+		//console.log(htmlCode.value);
+		determineValidHTML();
+	}
+
+	function escapeChars(codeEscaped) {
+	    codeEscaped = codeEscaped.replace(/</g, "&lt;");
+	    codeEscaped = codeEscaped.replace(/>/g, "&gt;");
+	    codeEscaped = codeEscaped.replace(/&/g, "&amp;");
+	    codeEscaped = codeEscaped.replace(/\'/g, "&#039;");
+	    codeEscaped = codeEscaped.replace(/\"/g, "&quot;");
+	    return codeEscaped;
+	}
 
 /*
 To be checked:
@@ -36,16 +53,17 @@ To be checked:
 
 */
 	function determineValidHTML() {
-		if(!doctypePresent()) {
+		var HTMLcodeContent = document.getElementById("htmlCodeOutput");
+		if(!doctypePresent(HTMLcodeContent)) {
 			HTMLcodeContent.innerHTML = "DOCTYPE missing, not valid XHTML";
-		} else if(properlyNested()){
-			console.log("properly nested");
+		} else if(properlyNested(HTMLcodeContent)){
+			HTMLcodeContent.innerHTML = "HTML Code Properly Nested";
 		} else {
-			console.log("not properly nested");
+			HTMLcodeContent.innerHTML = "HTML Code NOT Properly Nested";
 		}
 	}
 
-	function doctypePresent() {
+	function doctypePresent(HTMLcodeContent) {
 		if(HTMLcodeContent.innerHTML.includes("&amp;lt;!DOCTYPE")) {
 			return true;
 		} else {
@@ -53,7 +71,7 @@ To be checked:
 		}
 	}
 
-	function properlyNested() {
+	function properlyNested(HTMLcodeContent) {
 		var selfClosingTags = ["<area />", "<base />", "<br />", "<col />", "<command />", "<embed />", "<hr />", "<img />", "<input />", "<keygen />", "<link />", "<meta />", "<param />", "<source />", "<track />", "<wbr />"];
 		var stack = [];
 		for(var i = 0; i < HTMLcodeContent.innerHTML.length;) {
@@ -62,14 +80,14 @@ To be checked:
 			//console.log(currentTag);
 			if (currentTag.match(/&amp;lt;[a-z1-9]+&amp;gt;/)) { //opening tag
 				stack.push(currentTag);
-				console.log(currentTag);
+				//console.log(currentTag);
 			} else if(currentTag.match(/&amp;lt;\/[a-z1-9]+&amp;gt;/)) { //closing tag
 				var topElement = stack.pop();
-				console.log(topElement);
+				//console.log(topElement);
 				if(topElement.substring(8, topElement.indexOf("&amp;gt;")) !== currentTag.substring(9, currentTag.indexOf("&amp;gt;"))) {
-					console.log("opening tag" + topElement.substring(8, topElement.indexOf("&amp;gt;")));
-					console.log("closing tag" + currentTag.substring(9, topElement.indexOf("&amp;gt;")));
-					console.log("terminate" + currentTag);
+					//console.log("opening tag" + topElement.substring(8, topElement.indexOf("&amp;gt;")));
+					//console.log("closing tag" + currentTag.substring(9, topElement.indexOf("&amp;gt;")));
+					//console.log("terminate" + currentTag);
 					return false;
 				}
 			}
